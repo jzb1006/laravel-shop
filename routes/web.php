7ab323f4efb1d87a7ade5b12bd3fbabcf497f7a5
jzb1006 <1129990545@qq.com>
2019-06-29 17:04:40 +0800

    触发激活邮箱

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,15 @@ Route::get('/','PagesController@root')->name('root');
 Auth::routes();
 
 Route::group(['middleware'=>'auth'],function (){
+
     Route::get('/email_verified_notice','PagesController@emailVerifiedNotice')
           ->name('email_verified_notice');
 
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     //开始
     Route::group(['middleware'=>'email_verified'],function (){
-        Route::get('/test',function (){
-            return "sdf";
-        });
+
     });
+
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
 });
