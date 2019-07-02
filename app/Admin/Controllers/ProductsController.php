9103ddde44fa8d1d39bd6f2248b8f28b8915a513
jzb1006 <1129990545@qq.com>
2019-07-02 15:50:17 +0800

    后台管理 - 商品列表

---
+++ app/Admin/Controllers/ProductsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Product;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+class ProductsController extends Controller
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'App\Models\Product';
+
+
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+            $content->header('商品列表');
+            $content->body($this->grid());
+        });
+    }
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(Product::class, function (Grid $grid) {
+            $grid->id('ID')->sortable();
+            $grid->title('商品名称');
+            $grid->on_sale('已上架')->display(function ($value) {
+                return $value ? '是' : '否';
+            });
+            $grid->price('价格');
+            $grid->rating('评分');
+            $grid->sold_count('销量');
+            $grid->review_count('评论数');
+
+            $grid->actions(function ($actions) {
+                $actions->disableView();
+                $actions->disableDelete();
+            });
+            $grid->tools(function ($tools) {
+                // 禁用批量删除按钮
+                $tools->batch(function ($batch) {
+                    $batch->disableDelete();
+                });
+            });
+        });
+    }
+
+
+}
