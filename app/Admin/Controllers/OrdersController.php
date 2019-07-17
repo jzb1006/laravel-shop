e254f0a321db4eafe358d850a2e013602198ef23
jzb1006 <1129990545@qqq.com>
2019-07-17 22:19:11 +0800

    管理后台 - 订单详情

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -125,4 +125,12 @@ class OrdersController extends Controller
 
         return $form;
     }
+
+    public function show(Order $order){
+        return Admin::content(function (Content $content)use($order){
+            $content->header('查看订单');
+            // body 方法可以接受 Laravel 的视图作为参数
+            $content->body(view('admin.orders.show', ['order' => $order]));
+        });
+    }
 }
