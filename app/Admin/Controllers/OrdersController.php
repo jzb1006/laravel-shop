835ce29d7280ad10154ff28bd89abc182e4a6779
jzb1006 <1129990545@qqq.com>
2019-07-21 21:46:59 +0800

    管理后台 - 拒绝退款

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Exceptions\InvalidRequestException;
+use App\Http\Requests\Admin\HandleRefundRequest;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use Encore\Admin\Controllers\AdminController;
@@ -164,5 +165,26 @@ class OrdersController extends Controller
 
         // 返回上一页
         return redirect()->back();
+    }
+
+    public function handleRefund(Order $order,HandleRefundRequest $request){
+        // 判断订单状态是否正确
+        if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
+            throw new InvalidRequestException('订单状态不正确');
+        }
+
+        if($request->input('agree')){
+
+        }else{
+            $extra = $order->extra?:[];
+            $extra['refund_disagree_reason'] = $request->input('reason');
+
+            $order->update([
+                'refund_status'=>Order::REFUND_STATUS_PENDING,
+                'extra'=>$extra
+            ]);
+        }
+
+        return $order;
     }
 }
