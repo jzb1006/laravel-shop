87e3df20cde1de3b5b689d03e7867093de0015ff
jzb1006 <1129990545@qqq.com>
2019-07-23 22:48:50 +0800

    优化优惠卷模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -10,12 +10,12 @@ use Illuminate\Http\Request;
 class CouponCodesController extends Controller
 {
     //
-    public function show($code){
+    public function show($code,Request $request){
         //判断优惠券是否存在
         if(!$record = CouponCode::where('code',$code)->first()){
             throw new CouponCodeUnavailableException('优惠卷不存在');
         }
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
         return $record;
     }
 
