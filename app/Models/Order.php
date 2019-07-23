e2d97c1d2017a8267f1de4da41cb15f3c8e29c7a
jzb1006 <1129990545@qq.com>
2019-07-23 16:33:44 +0800

    管理后台 - 优惠券列表

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -83,6 +83,10 @@ class Order extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function couponCode(){
+        return $this->belongsTo(CouponCode::class);
+    }
+
     public function items(){
         return $this->hasMany(OrderItem::class);
     }
