ceb5c3d925fa02bb73b0e0d0e7d1dcc58a44426b
jzb1006 <1129990545@qq.com>
2019-07-27 15:53:16 +0800

    初始化众筹模型

---
+++ database/migrations/2019_07_27_073955_product_add_type.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProductAddType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+            $table->string('type')->default(\App\Models\Product::TYPE_NORMAL)->after('id')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+            $table->dropColumn('type');
+        });
+    }
+}
