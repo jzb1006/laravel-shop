ada702761a4748fde6c97addb9e6041300530d19
jzb1006 <1129990545@qq.com>
2019-08-04 16:28:23 +0800

    2019年8月4日

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,47 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+$params = [
+    'index'=>'products',
+    'type'=>'_doc',
+    'body'=>[
+        'query'=>[
+            'bool'=>[
+                'filter'=>[
+                    ['term'=>['on_sale'=>true]]
+                ],
+                'must'=>[
+                    [
+                        'multi_match'=>[
+                            'query'=>'内存条',
+                            'fields'=>[
+                                'title^3',
+                                'long_title^2',
+                                'category^3',
+                                'description',
+                                'skus.title',
+                                'skus.description',
+                                'properties.value'
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ],
+        'aggs'=>[
+            'properties'=>[
+                'nested'=>[
+                    'path'=>'properties'
+                ],
+                'aggs'=>[
+                    'properties'=>[
+                        'terms'=>[
+                            'field'=>'properties.name'
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
